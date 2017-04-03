68567d4a0dedab8d5ad944f52d1da50cfbd29914
Bryan Summers <bryan.summers@kinchipsystems.co>
2017-04-03 17:05:36 +0800

    Created Authors component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';  // Importing the Component Decorator from Angular
-import {CoursesComponent} from './courses.component' // Start searching in current folder. Don't use .ts extension
+import {CoursesComponent} from './courses.component'; // Start searching in current folder. Don't use .ts extension
+import {AuthorsComponent} from './authors.component';
 
 // Then call the decorator (which is a function, that takes an object of properties/attributes)
 @Component({
     selector: 'my-app',
-    template: '<h1>Yo yo yo yo yo !!</h1><courses></courses>',
-    directives: [CoursesComponent] // Must be imported at the top of the file
+    template: '<h1>Yo yo yo yo yo !!</h1><courses></courses><authors></authors>',
+    directives: [CoursesComponent, AuthorsComponent] // Must be imported at the top of the file
 })
 export class AppComponent {
 
